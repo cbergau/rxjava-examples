3fca894d654d13534c3518708ceb79e70b3097cf
Christian Bergau <Christian.Bergau@Arvato.com>
2019-03-06 13:51:57 +0100

    Oberservable Interval

---
+++ src/main/java/de/christianbergau/ObservableInterval.java
@@ -0,0 +1,28 @@
+package de.christianbergau;
+
+import io.reactivex.Observable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @see "http://reactivex.io/documentation/operators/interval.html"
+ */
+public class ObservableInterval {
+    public static void main(String... args) {
+        Observable
+                .interval(3, TimeUnit.SECONDS)
+                .timeInterval()
+                .take(3)
+                .subscribe(longTimed -> {
+                    System.out.println(longTimed);
+                });
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
